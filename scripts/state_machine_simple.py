38c696d2a3fcc370ef65fa7cd8950cfe4a6eca3f
Ka Woong Hyun <kwh950724@gmail.com>
2018-11-08 20:09:53 +0900

    Add launch and succeed first
    should add file io to save mode

--- scripts/state_machine_simple.py
+++ scripts/state_machine_simple.py
@@ -13,11 +13,6 @@ from sensor_msgs.msg import Image
 from darknet_ros_msgs.msg import CheckForObjectsAction, CheckForObjectsGoal, CheckForObjectsResult, CheckForObjectsFeedback
 from enum import Enum
 
-#class Sign(Enum):
-#	A = 1
-#	B = 2
-
-
 #define common state Mission
 class CommonMission(smach.State):
 	def __init__(self, client_name):
@@ -28,7 +23,6 @@ class CommonMission(smach.State):
 		rospy.loginfo('Executing state %s', self.client_name)
 		client = actionlib.SimpleActionClient(self.client_name, MissionPlannerAction)
 		client.wait_for_server()
-
 		goal = MissionPlannerGoal()
 		goal.mission = 1
 		client.send_goal(goal)
@@ -37,28 +31,6 @@ class CommonMission(smach.State):
 		client.wait_for_result()
 		rospy.loginfo('%s finish'%self.client_name)
 		return 'finish'
-
-class LaneDetecting(smach.State):
-	def __init__(self, client_name):
-		smach.State.__init__(self, outcomes=['finish'])
-		self.client_name = client_name
-		self.publisher = rospy.Publisher('lane_detector', Int8, queue_size = 10)
-		pub = self.publisher
-	def execute(self, userdata):
-		rospy.loginfo('Executing state %s', self.client_name)
-		pub.publish(1)
-		client = actionlib.SimpleActionClient(self.client_name, MissionPlannerAction)
-	 	client.wait_for_server()
-
-		goal = MissionPlannerGoal()
-		goal.mission = 1
-		client.send_goal(goal)
-		rospy.loginfo("send goal")
-		
-		client.wait_for_result()
-		pub.publish(0)
-		rospy.loginfo('%s finish'%self.client_name)
-		return 'finish'	
 
 class Parking(smach.State):
 	def __init__(self, client_name):
@@ -80,119 +52,140 @@ class Parking(smach.State):
 		return 'finish'	
 
 class TrafficLight(smach.State):
-	def __init__(self, client_name, prob):
+	def __init__(self, client_name, prob, standard_count):
 		smach.State.__init__(self, outcomes=['finish'])
 		self.client_name = client_name
-		self.subscriber = rospy.Subscriber('/usb_cam/image_raw', Image, self.imageCallback)
 		self.flag = 0
+		self.callback_flag = 1
 		self.min_prob = prob
+		self.traffic_light_count = standard_count
+		self.cur_count = 0
 
 	def imageCallback(self, data):
-		client = actionlib.SimpleActionClient(self.client_name, MissionPlannerAction)
-		client.wait_for_server()
-		goal = CheckForObjectsGoal()
-		goal.image = data
-		#goal.id
-		client.send_goal(goal)
-		rospy.loginfo("send goal")
+		if self.callback_flag == 1:
+			client = actionlib.SimpleActionClient(self.client_name, CheckForObjectsAction)
+			client.wait_for_server()
+			goal = CheckForObjectsGoal()
+			goal.image = data
+			#goal.id
+			client.send_goal(goal)
+			rospy.loginfo("send goal")
 
-		client.wait_for_result()
+			client.wait_for_result()
 
-		result = client.get_result()
-		rospy.loginfo('%s get result'%self.client_name)
+			result = client.get_result()
+			rospy.loginfo('%s get result'%self.client_name)
 
-		bounding_box = result.bounding_boxes[0]
-		probability = bounding_box.probability
-		sign_type = bounding_box.Class
-		if probability >= min_prob or sign_type == 'Go':
-			self.flag = 1
+			bounding_box = result.bounding_boxes.bounding_boxes[0]
+			probability = bounding_box.probability
+			sign_type = bounding_box.Class
+			if probability > self.min_prob and sign_type == 'Go':
+				self.flag = 1
 
 	def execute(self, userdata):
+		subscriber = rospy.Subscriber('/usb_cam/image_raw', Image, self.imageCallback)
 		rospy.loginfo('executing trafficlight classification')
 		r = rospy.Rate(100)
 		while not rospy.is_shutdown():
 			if self.flag == 1:
-				rospy.loginfo('Go sign identified')
+				self.cur_count = self.cur_count + 1
+				rospy.loginfo('Go sign identified %dtimes'%self.cur_count)
+				self.flag = 0
+			if self.cur_count > self.traffic_light_count:
+				rospy.loginfo('Go sign certainly identified')
+				self.callback_flag = 0
 				return 'finish'
 			r.sleep()
 		
 		
 class SignAB(smach.State):
-	def __init__(self, client_name, prob):
+	def __init__(self, client_name, prob, standard_count):
 		smach.State.__init__(self, outcomes=['finish'], output_keys=['sign_ab_output'])
 		self.client_name = client_name 
-		self.subscriber = rospy.Subscriber('/usb_cam/image_raw', Image, self.imageCallback)
 		self.flag = 0
+		self.callback_flag = 1
 		self.min_prob = prob
+		self.sign_ab_count = standard_count
+		self.a_count = 0
+		self.b_count = 0
 
 	def imageCallback(self, data):
-		client = actionlib.SimpleActionClient(self.client_name, MissionPlannerAction)
-		client.wait_for_server()
-		goal = CheckForObjectsGoal()
-		goal.image = data
-		#goal.id
-		client.send_goal(goal)
-		rospy.loginfo("send goal")
+		if self.callback_flag == 1:	
+			client = actionlib.SimpleActionClient(self.client_name, CheckForObjectsAction)
+			client.wait_for_server()
+			goal = CheckForObjectsGoal()
+			goal.image = data
+			#goal.id
+			client.send_goal(goal)
+			rospy.loginfo("send goal")
 
-		client.wait_for_result()
+			client.wait_for_result()
 
-		result = client.get_result()
-		rospy.loginfo('%s get result'%self.client_name)
+			result = client.get_result()
+			rospy.loginfo('%s get result'%self.client_name)
 
-		bounding_box = result.bounding_boxes[0]
-		probability = bounding_box.probability
-		sign_type = bounding_box.Class
-		if probability >= min_prob or sign_type == 'Parking_A':
-			#self.flag = Sign.A
-			self.flag = 1
-		elif probabilty >= min_prob or sign_type == 'Parking_B':
-			#self.flag = Sign.B
-			self.flag = 2
+			bounding_box = result.bounding_boxes.bounding_boxes[0]
+			probability = bounding_box.probability
+			sign_type = bounding_box.Class
+			if probability > self.min_prob and sign_type == 'Parking_a':
+				self.flag = 1
+			elif probabilty > self.min_prob and sign_type == 'Parking_b':
+				self.flag = 2
 
 	def execute(self, userdata):
+		self.subscriber = rospy.Subscriber('/usb_cam/image_raw', Image, self.imageCallback)
 		rospy.loginfo('executing parking_sign classification')
 		r = rospy.Rate(100)
 		while not rospy.is_shutdown():
 			if self.flag == 1:
-				rospy.info('Parking_A sign identified')
-				#userdata.sign_ab_output = Sign.A
+				self.a_count = self.a_count + 1
+				rospy.loginfo('Parking_a sign identified %dtimes'%self.a_count)
+				self.flag = 0
+			elif self.flag == 2:
+				self.b_count = self.b_count + 1
+				rospy.loginfo('Parking_b sign identified %dtimes'%self.b_count)
+				self.flag = 0
+			if self.a_count > self.sign_ab_count:
+				rospy.loginfo('Parking_a sign certainly identified')
 				userdata.sign_ab_output = 1
+				self.callback_flag = 0
 				return 'finish'
-			if self.flag == Sign.B:
-				rospy.info('Parking_B sign identified')
-				#userdata.sign_ab_output = Sign.B
+			if self.b_count > self.sign_ab_count:
+				rospy.loginfo('Parking_b sign certainly identified')
 				userdata.sign_ab_output = 2
+				self.callback_flag = 0
 				return 'finish'
 			r.sleep()
 
 # main
 def main():
 	rospy.init_node('smach_example_state_machine')
-
-	probability = 70.0
+	probability = rospy.get_param('~probability')
+	standard_count = rospy.get_param('~standard_count')
 
 	#create a smach state machine
 	sm = smach.StateMachine(outcomes=['success'])
 	sm.userdata.sm_counter = 0
 	
 	#open the container
+	#change darknet_ros_light -> darknet_ros
 	with sm:
-		smach.StateMachine.add('mission_start',TrafficLight('mission_start', probability), transitions= 
+		smach.StateMachine.add('mission_start',TrafficLight('darknet_ros_light', probability, standard_count), transitions= 
 {'finish':'narrow_path'})
 		smach.StateMachine.add('narrow_path',CommonMission('narrow_path'), transitions = 
 {'finish':'lane_detector'})
-		smach.StateMachine.add('lane_detector',LaneDetecting('lane_detector'), transitions = 
+		smach.StateMachine.add('lane_detector',CommonMission('lane_detector'), transitions = 
 {'finish':'u_turn'})
-		smach.StateMachine.add('u_turn',CommonMission('u_turn'), transitions =
+		smach.StateMachine.add('u_turn',CommonMission('u_turn_and_crosswalk_stop'), transitions =
 {'finish':'wait_traffic_light'})
-		smach.StateMachine.add('wait_traffic_light',TrafficLight('darknet_ros', probability), transitions = {'finish':'car_tracking'})
+		smach.StateMachine.add('wait_traffic_light',TrafficLight('darknet_ros_light', probability, standard_count), transitions = {'finish':'car_tracking'})
 		smach.StateMachine.add('car_tracking',CommonMission('car_tracking'), transitions = {'finish':'sign_ab'})
-		smach.StateMachine.add('sign_ab',SignAB('darknet_ros', probability), transitions = 
+		smach.StateMachine.add('sign_ab',SignAB('darknet_ros_ab', probability, standard_count), transitions = 
 {'finish':'parking'}, remapping = {'sign_ab_output':'sm_counter'})
 		smach.StateMachine.add('parking',Parking('parking'), transitions = 
-{'finish':'mission_finish'}, remapping	= {'sign_ab_input':'sm_counter'})
-		smach.StateMachine.add('mission_finish',CommonMission('mission_finish'),transitions=
-{'finish':'success'})
+{'finish':'success'}, remapping	= {'sign_ab_input':'sm_counter'})
+		#smach.StateMachine.add('mission_finish',CommonMission('mission_finish'),transitions=
+#{'finish':'success'})
 
 
 	sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
@@ -206,5 +199,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-
